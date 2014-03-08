8091e1c3a0fbf3421b1352029a88e8e50a42b0a8
Corey Leigh Latislaw <colabug@gmail.com>
2014-03-08 16:45:27 -0500

    Added a speech recognizer.

--- src/com/colabug/glass/reporting/ReportingActivity.java
+++ src/com/colabug/glass/reporting/ReportingActivity.java
@@ -1,11 +1,17 @@
 package com.colabug.glass.reporting;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import com.google.android.glass.app.Card;
+
+import java.util.List;
 
 public class ReportingActivity extends Activity
 {
+    private static final int SPEECH_REQUEST = 0;
+
     /**
      * Called when the activity is first created.
      */
@@ -20,5 +26,28 @@ public class ReportingActivity extends Activity
         card1.setFootnote( "I'm the footer!" );
         // Don't call this if you're using TimelineManager
         setContentView( card1.toView() );
+
+        displaySpeechRecognizer();
+    }
+
+    private void displaySpeechRecognizer()
+    {
+        Intent intent = new Intent( RecognizerIntent.ACTION_RECOGNIZE_SPEECH );
+        startActivityForResult( intent, SPEECH_REQUEST );
+    }
+
+    @Override
+    protected void onActivityResult( int requestCode,
+                                     int resultCode,
+                                     Intent data )
+    {
+        if ( requestCode == SPEECH_REQUEST && resultCode == RESULT_OK )
+        {
+            List<String> results = data.getStringArrayListExtra(
+            RecognizerIntent.EXTRA_RESULTS );
+            String spokenText = results.get( 0 );
+            // Do something with spokenText.
+        }
+        super.onActivityResult( requestCode, resultCode, data );
     }
 }
