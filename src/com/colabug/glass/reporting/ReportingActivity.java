416c1e8b21ca9df30dc6558ae513e5bc0074b025
Corey Leigh Latislaw <colabug@gmail.com>
2014-03-08 15:18:50 -0500

    Inital project

---
+++ src/com/colabug/glass/reporting/ReportingActivity.java
@@ -0,0 +1,17 @@
+package com.colabug.glass.reporting;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ReportingActivity extends Activity
+{
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate( Bundle savedInstanceState )
+    {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.main );
+    }
+}
