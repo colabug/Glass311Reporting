60a96a044f3939c5c0026cad750dfe54547ff992
Corey Leigh Latislaw <colabug@gmail.com>
2014-03-08 15:50:05 -0500

    Added a simple card with text

--- src/com/colabug/glass/reporting/ReportingActivity.java
+++ src/com/colabug/glass/reporting/ReportingActivity.java
@@ -2,6 +2,7 @@ package com.colabug.glass.reporting;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.google.android.glass.app.Card;
 
 public class ReportingActivity extends Activity
 {
@@ -12,6 +13,12 @@ public class ReportingActivity extends Activity
     public void onCreate( Bundle savedInstanceState )
     {
         super.onCreate( savedInstanceState );
-        setContentView( R.layout.main );
+
+        // Create a card with some simple text and a footer.
+        Card card1 = new Card( this );
+        card1.setText( "This card has a footer." );
+        card1.setFootnote( "I'm the footer!" );
+        // Don't call this if you're using TimelineManager
+        setContentView( card1.toView() );
     }
 }
